3478cf87628b4e8a96c310cf333f5b97381c169e
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-12-17 20:32:58 +0530

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,15 +8,11 @@ function Card() {
         <div className='relative w-60 h-72 rounded-[40px] px-8 py-10 bg-zinc-900/90 text-white p-5 overflow-scroll'>
         <MdOutlineAttachFile />
         <p className='text-sm leading-tight mt-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-        <br />
-        <br />
-        <br />
-        <br />
-        <div className='footer flex items-center justify-between '>
+        <div className='w-full h-10 footer flex items-center mt-20 justify-between '>
             <p className='text-sm'>.4mb</p>
             <MdOutlineDownloadForOffline size={20} color='grey'/>    
         </div>
-        <div className='w-full h-10 py-3 absolute bg-blue-600 bottom-0 left-0 text-white'>
+        <div className='w-full h-10 py-3 absolute bg-green-600 bottom-0 left-0 text-white'>
             <p className='text-center text-sm '>Download Now</p>
            
         </div>
