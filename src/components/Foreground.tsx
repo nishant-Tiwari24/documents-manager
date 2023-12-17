ae2420a90af3018526ed9040f80f1c22cd271a58
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-12-17 19:03:30 +0530

    Update Foreground.tsx

--- src/components/Foreground.tsx
+++ src/components/Foreground.tsx
@@ -3,21 +3,7 @@ import React from 'react'
 function Foreground() {
   return (
     <div className='fixed z-[3] top-0 left-0 w-full h-full'>
-        
-
-<div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"></h5>
-    </a>
-    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit expedita facilis aspernatur provident quis est consectetur maxime harum obcaecati placeat vero tenetur similique sed magnam consequuntur mollitia atque, sunt debitis corporis, ab illo quae voluptatum quo ipsam. Quos, necessitatibus. Maxime labore delectus veritatis voluptatem optio sapiente exercitationem illo impedit earum!</p>
-    <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-        Read more
-        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-        </svg>
-    </a>
-</div>
-
+        <div className='w-60 h-72 rounded-[20px] bg-zinc-900/90'>hey</div>
     </div>
   )
 }
