f90d6835c11f113dfe66a34cf738b134b9bfa97c
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-12-17 19:08:38 +0530

    👾 card component

--- src/components/Foreground.tsx
+++ src/components/Foreground.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Card from './Card'
 
 function Foreground() {
   return (
     <div className='fixed z-[3] top-0 left-0 w-full h-full'>
-        <div className='w-60 h-72 rounded-[20px] bg-zinc-900/90'>hey</div>
+        <Card/>
     </div>
   )
 }
