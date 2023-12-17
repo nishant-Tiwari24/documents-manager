7e12b85ce520f0b94126eb4b655ce766cd0e7fa6
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-12-17 19:50:52 +0530

    🤡 react icons

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { MdOutlineAttachFile } from "react-icons/md";
+import { MdOutlineDownloadForOffline } from "react-icons/md";
 
 function Card() {
   return (
     <div>
-        <div className='w-60 h-72 rounded-[20px] bg-zinc-900/90'>
-            
+        <div className='relative w-60 h-72 rounded-[40px] px-8 py-10 bg-zinc-900/90 text-white p-5 overflow-scroll'>
+        <MdOutlineAttachFile />
+        <p className='text-sm leading-tight mt-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+        <br />
+        <br />
+        <br />
+        <br />
+        <div className='footer flex items-center justify-between '>
+            <p className='text-sm'>.4mb</p>
+            <MdOutlineDownloadForOffline size={20} color='grey'/>    
+        </div>
+        <div className='w-full h-10 py-3 absolute bg-blue-600 bottom-0 left-0 text-white'>
+            <p className='text-center text-sm '>Download Now</p>
+           
+        </div>
         </div>
     </div>
   )
