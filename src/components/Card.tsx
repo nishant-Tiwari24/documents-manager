b27362154d2ff85d952fa5c3c7a161f474c4b031
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-12-17 20:39:58 +0530

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,7 +5,7 @@ import { MdOutlineDownloadForOffline } from "react-icons/md";
 function Card() {
   return (
     <div>
-        <div className='relative w-60 h-72 rounded-[40px] px-8 py-10 bg-zinc-900/90 text-white p-5 overflow-scroll'>
+        <div className='relative w-60 h-72 rounded-[40px] px-8 py-10 bg-zinc-900/90 text-gray-200 p-5 overflow-scroll border-solid border-[1px] border-zinc-500'>
         <MdOutlineAttachFile />
         <p className='text-sm leading-tight mt-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
         <div className='w-full h-10 footer flex items-center mt-20 justify-between '>
@@ -13,7 +13,7 @@ function Card() {
             <MdOutlineDownloadForOffline size={20} color='grey'/>    
         </div>
         <div className='w-full h-10 py-3 absolute bg-green-600 bottom-0 left-0 text-white'>
-            <p className='text-center text-sm '>Download Now</p>
+            <p className='text-center text-[12px] '>Download Now</p>
            
         </div>
         </div>
