3b891f325e6cd268795bba1aea2d94f402ed8a69
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-12-18 09:19:40 +0530

    🐍 multiple components

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import { MdOutlineAttachFile } from "react-icons/md";
 import { MdOutlineDownloadForOffline } from "react-icons/md";
+import { IoMdCloseCircleOutline } from "react-icons/io";
 
-function Card(props) {
+function Card({data}) {
   return (
     <div>
         <div className='relative w-60 h-72 rounded-[40px] px-8 py-10 bg-zinc-900/90 text-gray-200 p-5 overflow-scroll border-solid border-[1px] border-zinc-500'>
         <MdOutlineAttachFile />
-        <p className='text-sm leading-tight mt-5'></p>
+        <p className='text-sm leading-tight mt-5'>{data.desc}</p>
         <div className='w-full h-10 footer flex items-center mt-20 justify-between '>
-            <p className='text-sm'>.4mb</p>
-            <MdOutlineDownloadForOffline size={20} color='grey'/>    
+            <p className='text-sm'>{data.size}</p>
+             {data.close ? (<IoMdCloseCircleOutline size={20} color='grey' /> ): (<MdOutlineDownloadForOffline size={20} color='grey'/>) }
         </div>
-        <div className='w-full h-10 py-3 absolute bg-green-600 bottom-0 left-0 text-white'>
-            <p className='text-center text-[12px] '>Download Now</p>
-           
+        {data.tag.isOpen && (
+        <div className={`w-full h-10 py-3 absolute ${data.tag.tagColor === "blue" ? "bg-green-500" : "bg-violet-500"} bottom-0 left-0 text-white`}>
+        <p className='text-center text-[12px] '>{data.tag.tagTitle}</p>
         </div>
+        )}
         </div>
     </div>
   )
-}
+} 
 
 export default Card
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -2,19 +2,62 @@ import React from 'react'
 import Card from './Card'
 
 function Foreground() {
+  
   const data= [
-    {desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.", 
+    {desc: "Lorem ipsum dolor sit amet lorem20 elit.", 
     size:".4mb", 
-    close: "true",
-    tag: {isOpen:"true", tagTitle:"Download Now", tagColor:"green"},
-  }
+    close: true,
+    tag: {isOpen:true, tagTitle:"Download Now", tagColor:"blue"},
+  },
+  {desc: "Lorem ipsum dolor sit amet lorem20 elit.", 
+  size:".4mb", 
+  close: true,
+  tag: {isOpen:true, tagTitle:"Download Now", tagColor:"green"},
+},
+{desc: "Lorem ipsum dolor sit amet lorem20 elit.", 
+size:".4mb", 
+close: true,
+tag: {isOpen:true, tagTitle:"Download Now", tagColor:"green"},
+},
+{desc: "Lorem ipsum dolor sit amet lorem20 elit.", 
+size:".4mb", 
+close: true,
+tag: {isOpen:true, tagTitle:"Upload", tagColor:"blue"},
+},
+{desc: "Lorem ipsum dolor sit amet lorem20 elit.", 
+size:".4mb", 
+close: true,
+tag: {isOpen:true, tagTitle:"Download Now", tagColor:"blue"},
+},
+{desc: "Lorem ipsum dolor sit amet lorem20 elit.", 
+size:".4mb", 
+close: true,
+tag: {isOpen:true, tagTitle:"Upload", tagColor:"green"},
+},
+{desc: "Lorem ipsum dolor sit amet lorem20 elit.", 
+size:".4mb", 
+close: true,
+tag: {isOpen:true, tagTitle:"Download Now", tagColor:"blue"},
+},
+{desc: "Lorem ipsum dolor sit amet lorem20 elit.", 
+size:".4mb", 
+close: true,
+tag: {isOpen:true, tagTitle:"Upload", tagColor:"green"},
+},
+{desc: "Lorem ipsum dolor sit amet lorem20 elit.", 
+size:".4mb", 
+close: true,
+tag: {isOpen:true, tagTitle:"Download Now", tagColor:"blue"},
+},
+
   ];
   
   return (
-    <div className='fixed z-[3] top-0 left-0 w-full h-full'>
-        {data.map((item, index)=>(
-          <Card data={item}/>
-        ))}
+    <div className='fixed z-[3] top-0 left-0 w-full h-full flex py-10 px-2 gap-10  flex-wrap'>
+      {data.map((item,index)=>(
+        <Card data={item}
+        />
+      ))}
     </div>
   )
 }
